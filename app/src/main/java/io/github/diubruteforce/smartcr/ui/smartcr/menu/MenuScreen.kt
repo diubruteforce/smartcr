b089e7d9e005232da63b352b2dc9f188cc8e8363
zoha131 <abirhasanzoha@gmail.com>
2020-12-25 08:43:32 +0600

    Add Icon for menus screen

--- app/src/main/java/io/github/diubruteforce/smartcr/ui/smartcr/menu/MenuScreen.kt
+++ app/src/main/java/io/github/diubruteforce/smartcr/ui/smartcr/menu/MenuScreen.kt
@@ -1,14 +1,14 @@
 package io.github.diubruteforce.smartcr.ui.smartcr.menu
 
+import androidx.annotation.DrawableRes
 import androidx.compose.foundation.ScrollableColumn
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.outlined.Pages
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.vectorResource
 import dev.chrisbanes.accompanist.insets.navigationBarsPadding
+import io.github.diubruteforce.smartcr.R
 import io.github.diubruteforce.smartcr.ui.common.MenuRow
 import io.github.diubruteforce.smartcr.ui.theme.Margin
 import io.github.diubruteforce.smartcr.utils.extension.toPairList
@@ -25,10 +25,10 @@ fun MenuScreen(
 
         Menu.values().toPairList().forEach {
             MenuRow(
-                leftIcon = it.first.imageVector,
+                leftIcon = vectorResource(id = it.first.iconRes),
                 leftTitle = it.first.title,
                 leftOnClick = { onMenuClick(it.first) },
-                rightIcon = it.second.imageVector,
+                rightIcon = vectorResource(id = it.second.iconRes),
                 rightTitle = it.second.title,
                 rightOnClick = { onMenuClick(it.second) }
             )
@@ -36,13 +36,13 @@ fun MenuScreen(
     }
 }
 
-enum class Menu(val title: String, val imageVector: ImageVector) {
-    FIND_FACULTY("Find Faculty", Icons.Outlined.Pages),
-    FIND_COURSE("Find Courses", Icons.Outlined.Pages),
-    GET_RESOURCE("Get Resource", Icons.Outlined.Pages),
-    EXTRA_CLASS("Extra Class", Icons.Outlined.Pages),
-    EXAM_ROUTINE("Exam Routine", Icons.Outlined.Pages),
-    FEES_SCHEDULE("Fees Schedule", Icons.Outlined.Pages),
-    YOUR_PROFILE("Your Profile", Icons.Outlined.Pages),
-    APP_SETTING("App Setting", Icons.Outlined.Pages),
+enum class Menu(val title: String, @DrawableRes val iconRes: Int) {
+    FIND_FACULTY("Find Faculty", R.drawable.find_faculty),
+    FIND_COURSE("Find Courses", R.drawable.find_course),
+    GET_RESOURCE("Get Resource", R.drawable.find_resource),
+    EXTRA_CLASS("Extra Class", R.drawable.extra_class),
+    EXAM_ROUTINE("Exam Routine", R.drawable.exam_routine),
+    FEES_SCHEDULE("Fees Schedule", R.drawable.fees_schedule),
+    YOUR_PROFILE("Your Profile", R.drawable.student_profile),
+    APP_SETTING("App Setting", R.drawable.app_setting),
 }
